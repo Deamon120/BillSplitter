54d079fa400bf413755ede715a16cabc42ab2f3e
Damian Slominski <damianslominski993@wp.pl>
2024-12-31 13:37:52 +0100

    stage2

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -8,5 +8,14 @@ else:
     for _ in range(number_of_friends):
         name_friend = input()
         new_dict[name_friend] = 0
+
+    bill = int(input("Enter the total bill value:\n"))
+    if bill % number_of_friends == 0:
+        bill_for_all = bill // number_of_friends
+    else:
+        bill_for_all = round(bill / number_of_friends, 2)
+
+    for key in new_dict:
+        new_dict[key] = bill_for_all
     print()
     print(new_dict)
