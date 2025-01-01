fd7e730dafd8df92e11f342feb1cb4be01dbcdf2
Damian Slominski <damianslominski993@wp.pl>
2025-01-01 16:23:19 +0100

    stage3

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,3 +1,4 @@
+import random
 number_of_friends = int(input())
 print()
 if number_of_friends <= 0:
@@ -15,7 +16,10 @@ else:
     else:
         bill_for_all = round(bill / number_of_friends, 2)
 
-    for key in new_dict:
-        new_dict[key] = bill_for_all
-    print()
-    print(new_dict)
+
+    lucky = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
+    if lucky == "Yes":
+        random_key = random.choice(list(new_dict.keys()))
+        print(f"{random_key} is the lucky one!")
+    else:
+        print("No one is going to be lucky")
