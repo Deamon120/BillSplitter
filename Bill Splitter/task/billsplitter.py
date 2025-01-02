28c0587c3beb2b9eff5547ad77941076a3e5dd62
Damian Slominski <damianslominski993@wp.pl>
2025-01-02 11:19:38 +0100

    stage4

--- Bill Splitter/task/billsplitter.py
+++ Bill Splitter/task/billsplitter.py
@@ -1,5 +1,5 @@
 import random
-number_of_friends = int(input())
+number_of_friends = int(input("Enter the number of friends joining (including you):\n"))
 print()
 if number_of_friends <= 0:
     print("No one is joining for the party")
@@ -9,17 +9,35 @@ else:
     for _ in range(number_of_friends):
         name_friend = input()
         new_dict[name_friend] = 0
-
+    print()
     bill = int(input("Enter the total bill value:\n"))
     if bill % number_of_friends == 0:
         bill_for_all = bill // number_of_friends
     else:
         bill_for_all = round(bill / number_of_friends, 2)
 
-
+    print()
     lucky = input('Do you want to use the "Who is lucky?" feature? Write Yes/No:\n')
+    print()
     if lucky == "Yes":
         random_key = random.choice(list(new_dict.keys()))
         print(f"{random_key} is the lucky one!")
     else:
         print("No one is going to be lucky")
+    if lucky == "Yes":
+        if bill % (number_of_friends - 1) == 0:
+            bill_for_all = bill // (number_of_friends - 1)
+        else:
+            bill_for_all = round(bill / (number_of_friends - 1), 2)
+    else:
+        if bill % number_of_friends == 0:
+            bill_for_all = bill // number_of_friends
+        else:
+            bill_for_all = round(bill / number_of_friends, 2)
+
+    for key in new_dict:
+        new_dict[key] = bill_for_all
+        if lucky == "Yes":
+            new_dict[random_key] = 0
+    print()
+    print(new_dict)
